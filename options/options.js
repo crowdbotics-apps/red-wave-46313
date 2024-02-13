885a74d6ef66630bb575fef94b656cd416b7eebb
Crowdbotics <team@crowdbotics.com>
2024-02-13 18:09:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-wave-46313",
+  url: "https://red-wave-46313.botics.co",
+  api: "https://red-wave-46313.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
